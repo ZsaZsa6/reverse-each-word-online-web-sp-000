05f785b31c5143365713756b0d15d39bfbb91be0
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-05 01:17:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(string)
   array.each do |word|
     new_array << word.reverse
   end
-    new_string = new_array.join 
+    new_string = new_array.join " "
   end
   new_string
 end
