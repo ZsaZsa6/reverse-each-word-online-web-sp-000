47f3855453c416857e2a92682c183ea4163451e3
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-05 01:05:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require "pry"
 def reverse_each_word(string)
   array = string.split
   binding.pry
+  new_string = ""
   array.each do |word|
-    new_string "#{word}" = word.reverse
-    new_string << array.to_s
+    new_string += word.reverse
   end
-
+  new_string
 end
