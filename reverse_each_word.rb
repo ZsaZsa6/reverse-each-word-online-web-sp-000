7175ec4a78fd0adfed8770ece3fb2edd545ac2d9
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-05 01:06:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 require "pry"
 def reverse_each_word(string)
   array = string.split
-  # binding.pry
+
   new_string = ""
   array.each do |word|
     new_string += word.reverse
+      # binding.pry
   end
   new_string
 end
